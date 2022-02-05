812df657b2960fcaef4693286e68a1c17f4ff3ea
AlexandreAnacleto85 <48165981+AlexandreAnacleto85@users.noreply.github.com>
2022-02-05 16:44:32 +0200

    Add files via upload

---
+++ banco/TextDataBase.java
@@ -0,0 +1,21 @@
+package banco;
+
+public interface TextDataBase {
+    default void update() {
+
+    }
+
+    default void export() {
+
+    }
+
+    default void update(int numero) {
+
+    }
+
+    default void export(int numero) {
+
+    }
+
+}
+
